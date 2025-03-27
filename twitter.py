ada277e2e669bf87f718368bc9297f1360b7fc3e
GANDJIDON SENAN HERMIONE <gandjesenan@gmail.com>
2025-03-27 10:20:34 +0100

    Mise à jour de twitter.py : [Ajout du jeton porteur]

--- twitter.py
+++ twitter.py
@@ -1,12 +1,13 @@
 import tweepy
 
 # Remplace ces valeurs par tes clés API
-BEARER_TOKEN = "ton_bearer_token"
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAFXX0AEAAAAAp0Pg%2BCJEin979hFJS0injbEgLho%3DoA61Pc3bsfKgFNnpU1gpVBzBqDi0Di1Cx0hAd2csM180OxhVcs"
 
+# Authentification avec l'API Twitter
 client = tweepy.Client(bearer_token=BEARER_TOKEN)
 
 # Recherche des tweets contenant un mot-clé
-query = "Python -is:retweet lang:fr"
+query = "data science -is:retweet lang:fr"
 tweets = client.search_recent_tweets(query=query, max_results=10)
 
 for tweet in tweets.data:
